ac79a4707bdf2411ad28e95178271be65cf1326c
Urban Cvek <urban.cvek@shopster.io>
2017-01-07 17:06:21 +0100

    Initial commit.

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+// @flow
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Ezra extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Ezra
+        </Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('Ezra', () => Ezra);
